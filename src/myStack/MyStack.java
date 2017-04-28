b76a3a42b9ff2954531a22948b7f1102d873955c
HAHH9527 <1024830255@qq.com>
2017-04-28 12:10:13 +0800

    栈@ver1.1

--- src/myStack/MyStack.java
+++ src/myStack/MyStack.java
@@ -1,5 +1,12 @@
 package myStack;
 
+/**
+ * @version 1.1
+ * 
+ * @author HAHH9527
+ *
+ * @param <E>
+ */
 public class MyStack<E> {
     private int size;
     private A<E> top;
@@ -8,6 +15,11 @@ public class MyStack<E> {
 	size = 0;
     }
 
+    /**
+     * 入栈
+     * 
+     * @param obj
+     */
     public void push(E obj) {
 	A<E> temp = new A<E>(obj);
 	if (this.size == -1) {
@@ -19,10 +31,20 @@ public class MyStack<E> {
 	size++;
     }
 
+    /**
+     * 取得栈顶元素，不弹出
+     * 
+     * @return 栈顶元素
+     */
     public E peek() {
 	return top.getValue();
     }
 
+    /**
+     * 取得栈顶元素，并弹出
+     * 
+     * @return 被弹出的栈顶元素
+     */
     public E pop() {
 	A<E> temp = top;
 	top = top.getHead();
@@ -30,6 +52,12 @@ public class MyStack<E> {
 	return temp.getValue();
     }
 
+    /**
+     * 查找obj在此栈的第哪个位置
+     * 
+     * @param obj
+     * @return obj所在位置
+     */
     public int search(E obj) {
 	A<E> temp = top;
 	int count = size - 1;
@@ -43,12 +71,25 @@ public class MyStack<E> {
 	return count;
     }
 
+    /**
+     * 验证此栈是否为空栈
+     * 
+     * @return 空栈返回true
+     */
     public boolean empty() {
 	if (size == 0) {
 	    return true;
 	} else {
 	    return false;
 	}
+    }
+
+    /**
+     * 
+     * @return 此栈的长度
+     */
+    public int getSize() {
+	return size;
     }
 
 }
