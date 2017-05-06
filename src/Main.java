6936da5347bc13a63e491a4a63e4d62cbccfe614
10248 <10248@Tang-F117-F6>
2017-05-06 13:35:18 +0800

    测试方法

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;//导包
 public class Main {
 
     public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);// 生成一个对象接收输入
-	int x = sc.nextInt();// 接收一个整型
+	int x = 1024 * 1024;
+	for (int i = 0; i < x; i++) {
+	    System.out.print("a");
+	}
     }
 }
