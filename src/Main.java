13e0b4d6729ec731d764c9f9036e784d287f4025
HAHH9527 <1024830255@qq.com>
2017-05-20 12:42:02 +0800

    莫名其妙的一次提交

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
-import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	BigInteger x = new BigInteger("2");
-	int count = 1;
-	while (true) {
-	    x = x.multiply(x);
-	    count++;
-	    System.out.println(count + ":\n" + x.toString());
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNext()) {
+	    int x = sc.nextInt();
+	    int num[][] = new int[x][x];
+	    for (int m = 0; m < x; m++) {
+		for (int n = 0; n <= m; n++) {
+		    if (n == 0 || m == n) {
+			num[m][n] = 1;
+		    } else {
+			num[m][n] = num[m - 1][n - 1] + num[m - 1][n];
+		    }
+		}
+	    }
+	    for (int i = 0; i < x; i++) {
+		for (int j = x - i; j <= x; j++) {
+		    System.out.print(num[i][x - j]);
+		    if (j != x) {
+			System.out.print(" ");
+		    }
+		}
+		System.out.println("");
+	    }
+	    System.out.println("");
 	}
     }
 }
