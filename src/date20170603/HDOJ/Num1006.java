94522bce4c501f914996638f085e8d8e9273f06b
HAHH9527 <1024830255@qq.com>
2017-06-03 14:45:41 +0800

    2017年6月3日集训 http://acm.hdu.edu.cn/diy/contest_show.php?cid=31976

---
+++ src/date20170603/HDOJ/Num1006.java
@@ -0,0 +1,44 @@
+package date20170603.HDOJ;
+
+import java.util.Scanner;
+
+public class Num1006 {
+    private static boolean huiwen(int x) {
+	boolean ret = true;
+	String temp = String.valueOf(x);
+	char[] c = temp.toCharArray();
+	for (int i = 0; i < (c.length + 1) / 2; i++) {
+	    if (c[i] != c[c.length - 1 - i]) {
+		ret = false;
+		break;
+	    }
+	}
+	return ret;
+    }
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNext()) {
+	    int x = sc.nextInt();
+	    String out = "";
+	    out += x;
+	    int count = 0;
+	    while (true) {
+		if (huiwen(x)) {
+		    break;
+		}
+		char[] c = String.valueOf(x).toCharArray();
+		String strTemp = "";
+		for (int i = c.length - 1; i >= 0; i--) {
+		    strTemp += c[i];
+		}
+		int intTemp = Integer.valueOf(strTemp);
+		x += intTemp;
+		out += "--->" + x;
+		count++;
+	    }
+	    System.out.println(count);
+	    System.out.println(out);
+	}
+    }
+}
---
+++ src/date20170603/HDOJ/Num1007.java
@@ -0,0 +1,43 @@
+package date20170603.HDOJ;
+
+import java.util.Scanner;
+
+public class Num1007 {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNext()) {
+	    int n = sc.nextInt();
+	    for (int i = 0; i < n; i++) {
+		String password = sc.next();
+		if (password.length() < 8 || password.length() > 16) {
+		    System.out.println("NO");
+		    continue;
+		}
+		boolean[] flag = new boolean[4];
+		char[] temp = password.toCharArray();
+		for (char c : temp) {
+		    if (c >= 'A' && c <= 'Z') {
+			flag[0] = true;
+		    } else if (c >= 'a' && c <= 'z') {
+			flag[1] = true;
+		    } else if (c >= '0' && c <= '9') {
+			flag[2] = true;
+		    } else {
+			flag[3] = true;
+		    }
+		}
+		int count = 0;
+		for (boolean b : flag) {
+		    if (b) {
+			count++;
+		    }
+		}
+		if (count >= 3) {
+		    System.out.println("YES");
+		} else {
+		    System.out.println("NO");
+		}
+	    }
+	}
+    }
+}
