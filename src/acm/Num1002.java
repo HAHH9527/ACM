2f5e97fe5ee3f40f531cce6d1467d6580058198a
HAHH9527 <1024830255@qq.com>
2017-09-06 18:08:09 +0800

    HDUOJ1000 1002 2096题

---
+++ src/acm/Num1002.java
@@ -0,0 +1,20 @@
+package acm;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Num1002 {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int l = sc.nextInt();
+	for (int i = 1; i <= l; i++) {
+	    if (i != 1)
+		System.out.println();
+	    BigInteger a, b;
+	    a = sc.nextBigInteger();
+	    b = sc.nextBigInteger();
+	    System.out.println("Case " + i + ":");
+	    System.out.println(a + " + " + b + " = " + a.add(b));
+	}
+    }
+}
