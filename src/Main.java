bc2b7fedd41e4d292be08a53d7f781accd3b64cf
HAHH9527 <1024830255@qq.com>
2017-09-16 08:23:50 +0800

    test save action on IDEA

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        if (true) {
-            System.out.println(true);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("yes");
         }
     }
 }
