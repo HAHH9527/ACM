d40c154b35c023e7c1221f034a47e539a9795ce3
9527Number <1024830255@qq.com>
2017-11-29 21:53:53 +0800

    hduOJ 1235 1279

---
+++ src/hduOJ/Num1279.java
@@ -0,0 +1,35 @@
+package hduOJ;
+
+import java.util.Scanner;
+
+public class Num1279 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int m = sc.nextInt();
+            for (int i = 0; i < m; i++) {
+                int n = sc.nextInt();
+                int t = 0;
+                while (n != 1) {
+                    if (n % 2 == 1) {
+                        if (t == 0) {
+                            System.out.print(n);
+                        } else {
+                            System.out.print(" " + n);
+                        }
+                        t++;
+                        n = n * 3 + 1;
+                    } else {
+                        n = n / 2;
+                    }
+                }
+                if (t == 0) {
+                    System.out.println("No number can be output !");
+                } else {
+                    System.out.println("");
+                }
+            }
+        }
+    }
+}
