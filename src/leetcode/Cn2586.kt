3d3246b86bff1f655ee894f4b8f80eafd5cb35f7
Tang <1024830255@qq.com>
2023-11-07 10:48:49 +0800

    leetcode-cn: 2586. 统计范围内的元音字符串数

--- src/leetcode/Cn2586.kt
+++ src/leetcode/Cn2586.kt
@@ -13,5 +13,17 @@ object Cn2586 {
             }
             return count
         }
+
+        fun vowelStrings2(words: Array<String>, left: Int, right: Int): Int {
+            var count = 0
+            for (i in left..right) {
+                if (words[i].first().isVowel() && words[i].last().isVowel()) count++
+            }
+            return count
+        }
+
+        fun Char.isVowel(): Boolean {
+            return this == 'a' || this == 'e' || this == 'i' || this == 'o' || this == 'u'
+        }
     }
 }
